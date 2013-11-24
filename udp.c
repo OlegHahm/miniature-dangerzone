6af703656099187c4c9b44f0d7f0aa3ff4d16e00
Oleg Hahm <oleg@hobbykeller.org>
2013-11-24 15:55:44 +0100

    improved message when sending a UDP packet

--- udp.c
+++ udp.c
@@ -100,7 +100,7 @@ void udp_send(char *str)
         printf("Error sending packet!\n");
     }
     else {
-        printf("Successful sent %i bytes over UDP to %s\n", bytes_sent, ipv6_addr_to_str(addr_str, &ipaddr));
+        printf("Successful deliverd %i bytes over UDP to %s to 6LoWPAN\n", bytes_sent, ipv6_addr_to_str(addr_str, &ipaddr));
     }
 
     destiny_socket_close(sock);
