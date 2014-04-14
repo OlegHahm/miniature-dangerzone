47ed3fcf238d65581f39b9a6fda8a4c2b02bafb9
Oleg Hahm <oleg@hobbykeller.org>
2014-04-14 14:28:41 +0200

    simple blinker should simply blink

--- simple-blinker/main.c
+++ simple-blinker/main.c
@@ -5,40 +5,19 @@
 #include "ps.h"
 #include "board.h"
 
-char mystack[KERNEL_CONF_STACKSIZE_PRINTF];
-
-void mythread(void) {
-    puts("go!");
-    thread_sleep();
-    puts("second thread: back in business");
-    puts("set timer in thread 2");
-    hwtimer_wait(1000);
-    puts("Going to sleep again");
-    thread_sleep();
-}
-
 int main(void) {
-    int i, pid;
 
-    pid = thread_create(mystack, KERNEL_CONF_STACKSIZE_PRINTF, PRIORITY_MAIN+1, CREATE_STACKTEST, mythread, "second");
-    if (pid < 0) {
-        puts("error creating thread");
+    LED_RED_OFF;
+    LED_GREEN_OFF;
+    LED_ORANGE_OFF;
+
+    while (1) {
+        hwtimer_wait(HWTIMER_TICKS(100 * 1000));
+        LED_RED_TOGGLE;
+        hwtimer_wait(HWTIMER_TICKS(200 * 1000));
+        LED_GREEN_TOGGLE;
+        hwtimer_wait(HWTIMER_TICKS(400 * 1000));
+        LED_ORANGE_TOGGLE;
     }
-    else { 
-        puts("foo");
-        for (i = 0; i < 10; i++) {
-            puts("set timer in thread 1");
-            hwtimer_wait(10 * 1000);
-            if (i == 1) {
-                thread_wakeup(pid);
-            }
-        }
-        puts("bar");
-        thread_print_all();
-        thread_sleep();
-    }
-
-    while (1);
-
     return 0;
 }
