44493d9e8d1cda0557e5380097b4de95a7dd7e51
Oleg Hahm <oleg@hobbykeller.org>
2014-09-24 23:29:16 +0200

    sched_testing: changed the purpose of this application

--- sched_testing/main.c
+++ sched_testing/main.c
@@ -6,24 +6,20 @@ char snd_thread_stack[KERNEL_CONF_STACKSIZE_MAIN];
 void *snd_thread(void *unused)
 {
     (void) unused;
-    puts("snd_thread running, going to sleep");
-    thread_sleep();
-    puts("snd_thread awake again");
+    puts("snd_thread running");
     return NULL;
 }
 
 int main(void)
 {
     puts("Starting Sched testing");
-    kernel_pid_t snd_pid = thread_create(snd_thread_stack,
-                                         sizeof(snd_thread_stack),
-                                         PRIORITY_MAIN, 0,
-                                         snd_thread, NULL, "snd");
+    thread_create(snd_thread_stack, sizeof(snd_thread_stack), PRIORITY_MAIN,
+                  CREATE_WOUT_YIELD, snd_thread, NULL, "snd");
 
-    puts("yield");
+    puts("yield 1");
     thread_yield();
-    puts("wake up second thread");
-    thread_wakeup(snd_pid);
+    puts("yield 2");
+    thread_yield();
     puts("terminating main thread");
 
     return 0;
