c31e7e8276240d516c8771eb5d2cfc24ac20be5f
Oleg Hahm <oleg@hobbykeller.org>
2014-09-24 23:25:08 +0200

    sched_testing: yield before waking up

--- sched_testing/main.c
+++ sched_testing/main.c
@@ -6,7 +6,9 @@ char snd_thread_stack[KERNEL_CONF_STACKSIZE_MAIN];
 void *snd_thread(void *unused)
 {
     (void) unused;
-    puts("snd_thread running");
+    puts("snd_thread running, going to sleep");
+    thread_sleep();
+    puts("snd_thread awake again");
     return NULL;
 }
 
@@ -15,9 +17,11 @@ int main(void)
     puts("Starting Sched testing");
     kernel_pid_t snd_pid = thread_create(snd_thread_stack,
                                          sizeof(snd_thread_stack),
-                                         PRIORITY_MAIN, CREATE_SLEEPING,
+                                         PRIORITY_MAIN, 0,
                                          snd_thread, NULL, "snd");
 
+    puts("yield");
+    thread_yield();
     puts("wake up second thread");
     thread_wakeup(snd_pid);
     puts("terminating main thread");
