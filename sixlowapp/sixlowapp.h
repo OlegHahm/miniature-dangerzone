44724f69125c7ce8399650653f17fd3750f8996a
Oleg Hahm <oleg@hobbykeller.org>
2014-11-06 22:24:14 +0100

    sixlowapp: splitted into more files

--- sixlowapp/sixlowapp.h
+++ sixlowapp/sixlowapp.h
@@ -4,12 +4,20 @@
 #include "kernel.h"
 #include "ipv6.h"
 
-#define APP_VERSION "1.2"
+/**
+ * @brief   The application version number
+ */
+#define APP_VERSION "1.3"
 
 /**
  * @brief   Which interface should be used for 6LoWPAN
  */
 #define IF_ID   (0)
+
+/**
+ * @brief   Define a IPC message type for ICMP echo reply handling
+ */
+#define ICMP_ECHO_REPLY_RCVD    (4444)
 
 /**
  * @brief   PID for UDP server thread
@@ -20,6 +28,16 @@ extern kernel_pid_t sixlowapp_udp_server_pid;
  * @brief   UDP port number that netcat uses to listen at
  */
 extern uint16_t sixlowapp_netcat_listen_port;
+
+/**
+ * @brief   Marker if we're waiting for an ICMP echo reply
+ */
+extern unsigned sixlowapp_waiting_for_pong;
+
+/**
+ * @brief   The PID of the thread waiting for the ICMP echo reply
+ */
+extern kernel_pid_t sixlowapp_waiter_pid;
 
 /**
  * @brief   Helper variable for IP address printing
@@ -66,5 +84,25 @@ void *sixlowapp_monitor(void *unused);
  * @param[in] arg   Obsolete
  */
 void *sixlowapp_udp_server_loop(void *arg);
+
+/**
+ * @brief           Provides a workaround for currently broken 6LoWPAN ND
+ *
+ * @param[in] dest  The IPv6 address to add to the neighbor cache
+ */
+void sixlowapp_ndp_workaround(ipv6_addr_t *dest);
+
+/**
+ * @brief               Waits for a certain message type for a given time span
+ *
+ * @param[out] m        Pointer to preallocated ``msg_t`` structure, must not
+ *                      be NULL
+ * @param[in] timeout   The maximum interval to wait
+ * @param[in] mtype     The message type to wait for
+ *
+ * @return  0 if no message of type @p mtype was received
+ * @return  The number of microseconds before the message was received
+ */
+uint64_t sixlowapp_wait_for_msg_type(msg_t *m, timex_t timeout, uint16_t mtype);
 
 #endif /* SIXLOWAPP_H */
