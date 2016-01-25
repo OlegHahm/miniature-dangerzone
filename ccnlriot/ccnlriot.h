a599dd3693167909b6c17baee179e061555eb168
Oleg Hahm <oleg@hobbykeller.org>
2016-01-25 11:23:28 +0100

    ccnlriot: some config updates

--- ccnlriot/ccnlriot.h
+++ ccnlriot/ccnlriot.h
@@ -3,14 +3,21 @@
 
 #define CCNLRIOT_NETIF      (3)
 
-#define CCNLRIOT_CHANNEL    (26)
+#define CCNLRIOT_CHANNEL    (17)
 
-#define CCNLRIOT_INT_RETRIES    (3)
+#define CCNLRIOT_INT_RETRIES    (10)
+
+#define CCNLRIOT_CSMA_RETRIES   (3)
+
+#define CCNLRIOT_CONSUMERS      (5)
+
+#define CCNLRIOT_LOGLEVEL  (LOG_LEVEL)
 
 #define CCNLRIOT_PREFIX1    "/riot/peter/schmerzl"
 #define CCNLRIOT_PREFIX2    "/start/the/riot"
 
-#define CCNLRIOT_CONT       "#######-----------*******--------############"
+#define CCNLRIOT_CONT       "#########-----------**********--------############"
+//#define CCNLRIOT_CONT       "#####"
 
 #define CCNLRIOT_BUF_SIZE (128)
 
@@ -45,5 +52,7 @@ extern uint8_t ccnlriot_id[CCNLRIOT_NUMBER_OF_NODES][CCNLRIOT_ADDRLEN];
 void ccnlriot_routes_setup(void);
 int ccnlriot_routes_add(char *pfx, uint8_t *relay_addr, size_t addr_len);
 int ccnlriot_get_mypos(void);
+
+int ccnlriot_stats(int argc, char **argv);
 
 #endif /* CCNLRIOT_H */
