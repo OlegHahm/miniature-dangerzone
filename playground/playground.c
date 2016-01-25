57a250d5951c13dee2401d84ecdfb78b917c6bb9
Oleg Hahm <oleg@hobbykeller.org>
2016-01-25 03:46:19 +0100

    playground: periodic gettimeofday via xtimer

--- playground/playground.c
+++ playground/playground.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include "xtimer.h"
+
 int main(void)
 {
-    puts(1, "foobar\n", 8);
+    int r;
+    struct timeval tp;
+    for (int i = 0; i < 100; i++) {
+        r = gettimeofday(&tp, NULL);
+        printf("s: %li, us: %li (%i)\n", tp.tv_sec, tp.tv_usec, r);
+        xtimer_sleep(1);
+    }
     return 0;
 }
